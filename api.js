37743704a0796c3613a4363b51a72e78bddd75fc
dsp9107 <dsp9107@outlook.com>
2021-07-23 19:14:50 +0000

    prettier + pkgs

--- api.js
+++ api.js
@@ -1,19 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = 9107;
+const PORT = process.env.PORT;
 
 // Middleware
 
 app.use((req, res, next) => {
-    console.log(
-        "Request Received: ",
-        new Date().toLocaleString("en-US", {
-            timeZone: "Asia/Calcutta",
-        })
-    );
-    next();
+  console.log(
+    "Request Received: ",
+    new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Calcutta",
+    })
+  );
+  next();
 });
 
 app.use(morgan("dev"));
@@ -21,28 +23,45 @@ app.use(express.json());
 
 //Routes
 
+app.get("/", (req, res) => {
+  res.json([
+    {
+      method: "GET",
+      path: "/queryParams",
+    },
+    {
+      method: "POST",
+      path: "/bodyJSON",
+    },
+    {
+      method: "GET",
+      path: "/pathVars/:ping",
+    },
+  ]);
+});
+
 app.get("/queryParams", (req, res) => {
-    res.send(req.query);
+  res.send(req.query);
 });
 
 app.post("/bodyJSON", (req, res) => {
-    res.json(req.body);
+  res.json(req.body);
 });
 
 app.get("/pathVars/:ping", (req, res) => {
-    res.send(req.params.ping);
+  res.send(req.params.ping);
 });
 
 //Error Handling
 
 app.use((req, res, next) => {
-    const error = new Error("Not Found");
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
 //Server Config
 
 var server = app.listen(PORT, () => {
-    console.log(`Listening At ${PORT} ...`);
+  console.log(`Listening At ${PORT} ...`);
 });
